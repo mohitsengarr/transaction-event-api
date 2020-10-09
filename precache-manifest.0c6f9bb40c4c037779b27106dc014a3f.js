9ff2ad165297ea7f345bd91dc24a92231b7b70e6
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-10-09 15:03:24 +0000

    Deploying to gh-pages from  @ c4c1bd2bded6dde2106b3c8b9862572c2b05e9fe 🚀

---
+++ precache-manifest.0c6f9bb40c4c037779b27106dc014a3f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c06b559822fff63c4bcb8f28c6f932",
+    "url": "/transaction-event-api/index.html"
+  },
+  {
+    "revision": "c41cc625872a06e6206f",
+    "url": "/transaction-event-api/static/css/2.ecb7c289.chunk.css"
+  },
+  {
+    "revision": "19856e3c50d36764a813",
+    "url": "/transaction-event-api/static/css/main.d2866852.chunk.css"
+  },
+  {
+    "revision": "c41cc625872a06e6206f",
+    "url": "/transaction-event-api/static/js/2.da773f90.chunk.js"
+  },
+  {
+    "revision": "cccc6d91b5e64164365f6c4135751a26",
+    "url": "/transaction-event-api/static/js/2.da773f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19856e3c50d36764a813",
+    "url": "/transaction-event-api/static/js/main.30aca523.chunk.js"
+  },
+  {
+    "revision": "a19debf19a47611f273e",
+    "url": "/transaction-event-api/static/js/runtime-main.3003cedc.js"
+  },
+  {
+    "revision": "37025c479538428cb667cc91b341e375",
+    "url": "/transaction-event-api/static/media/api.37025c47.yaml"
+  },
+  {
+    "revision": "8503c64fa486375dc20b3c00aa2e250a",
+    "url": "/transaction-event-api/static/media/download-icon-white.8503c64f.svg"
+  },
+  {
+    "revision": "6332dd8cb99e02f089de4f747566362c",
+    "url": "/transaction-event-api/static/media/drop-grey.6332dd8c.svg"
+  },
+  {
+    "revision": "813b1258bed1257b91bcff7bf0282fbc",
+    "url": "/transaction-event-api/static/media/drop-white.813b1258.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/transaction-event-api/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "6c8e57277f41d456ad53ae6fcbc74b80",
+    "url": "/transaction-event-api/static/media/logo.6c8e5727.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/transaction-event-api/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/transaction-event-api/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/transaction-event-api/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/transaction-event-api/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/transaction-event-api/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
