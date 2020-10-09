d4a1753c8c1fe6b8e37793ce369590a7adf814e8
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-10-09 15:52:23 +0100

    Update JsonSerialiser.cs

--- TransactionEventApi.Business/Serialisation/JsonSerialiser.cs
+++ TransactionEventApi.Business/Serialisation/JsonSerialiser.cs
@@ -10,7 +10,7 @@ namespace Glasswall.Administration.K8.TransactionEventApi.Business.Serialisation
         public TObject Deserialise<TObject>(string jsonString)
         {
             if (jsonString == null) throw new ArgumentNullException(nameof(jsonString));
-
+             
             return JsonConvert.DeserializeObject<TObject>(jsonString);
         }
     }
