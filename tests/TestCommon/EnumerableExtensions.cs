c5596612846d525fe6d2aa5c445033e9ce399aad
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-10-14 14:50:38 +0100

    Add Test common project to correct folder

---
+++ tests/TestCommon/EnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestCommon
+{
+    public static class EnumerableExtensions
+    {
+        public static async IAsyncEnumerator<TItem> AsAsyncEnumerator<TItem>(this IEnumerable<TItem> items)
+        {
+            foreach (var item in items)
+                yield return item;
+
+            await Task.CompletedTask;
+        }
+
+        public static async Task<IEnumerable<TItem>> AsEnumerableAsync<TItem>(this IAsyncEnumerable<TItem> items)
+        {
+            var ret = new List<TItem>();
+
+            await foreach (var item in items)
+            {
+                ret.Add(item);
+            }
+
+            return ret;
+        }
+    }
+}
