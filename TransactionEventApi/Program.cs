8bfd96385019081a14c4c2f1ee5cf379e70a16be
PEpetra <61695423+PEpetra@users.noreply.github.com>
2020-10-19 16:44:35 +0100

    Update Program.cs

--- TransactionEventApi/Program.cs
+++ TransactionEventApi/Program.cs
@@ -9,6 +9,7 @@ namespace Glasswall.Administration.K8.TransactionEventApi
     {
         public static void Main(string[] args)
         {
+        string password = "123456";
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,3 +21,6 @@ namespace Glasswall.Administration.K8.TransactionEventApi
                 });
     }
 }
+
+
+
