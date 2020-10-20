efd01a97fc31c8b88a7f9f5e80839e7ca3a77e5c
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-10-20 10:11:52 +0100

    Update ConfigurationBindException.cs

--- TransactionEventApi.Business/Configuration/ConfigurationBindException.cs
+++ TransactionEventApi.Business/Configuration/ConfigurationBindException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using Glasswall.Administration.K8.TransactionEventApi.Common.Configuration.Validation.Errors;
 
 namespace Glasswall.Administration.K8.TransactionEventApi.Business.Configuration
@@ -11,6 +12,11 @@ namespace Glasswall.Administration.K8.TransactionEventApi.Business.Configuration
         public ConfigurationBindException(IEnumerable<ConfigurationParserError> errors)
             : base("Error binding configuration: " + string.Join(Environment.NewLine, errors.Select(error => $"{error.ParamName} - {error.Reason}")))
         {
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
