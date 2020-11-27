c3961bc826031db86d4215dcb874e3f2dce0f706
Sebastian Birch <sbirch@glasswallsolutions.com>
2020-11-27 16:25:21 +0000

    Add Self Signed SSL

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 #See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
 
-FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-buster-slim AS base
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine AS base
 WORKDIR /app
-EXPOSE 80
 EXPOSE 443
+EXPOSE 80
 
-FROM mcr.microsoft.com/dotnet/core/sdk:3.1-buster AS build
+RUN apk update && \
+apk add --no-cache openssl && \
+openssl req -x509 -nodes -days 365 -subj "/C=GB/ST=England/O=Glasswall Solutions Ltd/CN=transaction-event-api.com" -newkey rsa:2048 -keyout /etc/ssl/private/server.key -out /etc/ssl/certs/server.crt;
+
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1-alpine AS build
 WORKDIR /src
 COPY ["TransactionEventApi/TransactionEventApi.csproj", "TransactionEventApi/"]
 COPY ["TransactionEventApi.Business/TransactionEventApi.Business.csproj", "TransactionEventApi.Business/"]
@@ -21,4 +25,6 @@ RUN dotnet publish "TransactionEventApi.csproj" -c Release -o /app/publish
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
+COPY --from=base /etc/ssl/private/server.key /etc/ssl/private/
+COPY --from=base /etc/ssl/certs/server.crt /etc/ssl/certs/
 ENTRYPOINT ["dotnet", "TransactionEventApi.dll"]
--- TransactionEventApi/Program.cs
+++ TransactionEventApi/Program.cs
@@ -1,5 +1,11 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Glasswall.Administration.K8.TransactionEventApi
@@ -16,8 +22,40 @@ namespace Glasswall.Administration.K8.TransactionEventApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.AddServerHeader = false;
+                        options.ConfigureHttpsDefaults(opts =>
+                        {
+                            opts.ServerCertificate = LoadCertificate("/etc/ssl/certs/server.crt", "/etc/ssl/private/server.key");
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static X509Certificate2 LoadCertificate(string crtPath, string keyPath)
+        {
+            Console.WriteLine("Loading Self-Signed Certificate");
+
+            const string dashes = "-----";
+            var keyPem = File.ReadAllText(keyPath);
+            var index0 = keyPem.IndexOf(dashes, StringComparison.Ordinal);
+            var index1 = keyPem.IndexOf('\n', index0 + dashes.Length);
+            var index2 = keyPem.IndexOf(dashes, index1 + 1, StringComparison.Ordinal);
+            var keyDer = Convert.FromBase64String(keyPem.Substring(index1, index2 - index1));
+            X509Certificate2 certWithKey;
+
+            using (var certOnly = new X509Certificate2(crtPath))
+            using (var rsa = RSA.Create())
+            {
+                // For "BEGIN PRIVATE KEY"
+                rsa.ImportPkcs8PrivateKey(keyDer, out _);
+                certWithKey = certOnly.CopyWithPrivateKey(rsa);
+            }
+
+            Console.WriteLine("Using cert: " + certWithKey);
+            return certWithKey;
+        }
     }
 }
 
